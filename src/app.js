f24b2d1d206b49460b0f3706d921178b3116a1ef
Nilipta <niliptasatapathy@gmail.com>
2019-12-14 19:11:01 +0530

    only node express and ejs

---
+++ src/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const morgan = require('morgan');
+
+//settings
+app.set('port', 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+//Middlewares
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended:false}));
+
+
+//Routes
+app.use(require('./routes/index'));
+
+//Static
+app.use(express.static(path.join(__dirname,'public')));
+
+//add handler
+app.use((req, res, next) => {
+    res.render('404.ejs')
+})
+
+module.exports = app;
---
+++ src/routes/index.js
@@ -0,0 +1,54 @@
+const { Router } = require('express');
+const router = Router();
+const fs = require('fs');
+const uuid = require('uuid/v4');
+
+let books=[];
+try{
+    const readBookJson = fs.readFileSync('/tmp/books.json', 'utf-8');
+    books=JSON.parse(readBookJson);
+}
+catch(e)
+{
+    console.log("no books already staring new data store");
+}
+
+router.get('/', (req, res) => {
+    res.render('index.ejs', { books });
+})
+
+router.get('/new-entry', (req, res) => {
+    res.render('new-entry.ejs');
+})
+
+router.post('/new-entry', (req, res) => {
+    //console.log(req.body); //from the form entry
+    const {title, author ,image, description } = req.body;
+    if(!title || !author || !image || !description)
+    {
+        res.status(404).send('Entries incomlplete');
+        return;
+    }
+    let newBook = {
+        id: uuid(),
+        title,
+        author,
+        image,
+        description
+    }
+    books.push(newBook);
+
+    const json_books = JSON.stringify(books);
+    fs.writeFileSync('/tmp/books.json', json_books, 'utf-8');
+
+    res.redirect('/');
+})
+
+router.get('/delete/:id', (req, res) => {
+    books = books.filter(book => { book.id !== req.param.id})
+    const json_books = JSON.stringify(books);
+    fs.writeFileSync('/tmp/books.json', json_books, 'utf-8');
+    res.redirect('/');
+})
+
+module.exports = router;
